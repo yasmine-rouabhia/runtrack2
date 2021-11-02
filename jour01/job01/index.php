9a4ce28443c3a3bf3c3748a7a7e791d78aa330c9
yasmine-rouabhia <yasmine.rouabhia@laplateforme.io>
2021-11-02 15:33:49 +0100

    deuxieme etape

---
+++ jour01/job01/index.php
@@ -0,0 +1,21 @@
+<?php
+$str = "LaPlateforme"; // String chaine de caractère
+$str2 = "Vive"; // String 
+$str3 = "!"; // String
+
+echo $str2, "&nbsp", $str, "&nbsp", $str3;
+
+$val = 6; // valeur entier
+echo $val;
+
+$val = 4; // valeur entier 
+echo $val;
+echo "<br>";
+
+$myBool = true; //vrai 
+echo $myBool;
+
+$myBool = false; //faux
+echo $myBool;
+
+?>
