71db82c3af22930439b8b27e421eb5fd2578e7fd
yasmine-rouabhia <yasmine.rouabhia@laplateforme.io>
2021-11-03 17:34:39 +0100

    job03 fini

---
+++ jour02/job03/index.php
@@ -0,0 +1,21 @@
+<?php
+$var = 0;
+
+while($var <= 100){
+    $var ++;
+
+    if($var <= 20){
+     echo "<i>$var</i>". "<br>";
+    }
+
+    elseif($var == 42){
+        echo "LaPlateforme". "<br>";
+    }
+    elseif(($var >=25)&&($var <=50)){
+        echo "<u>$var</u>" ."<br>";
+    }
+    else{
+        echo $var."<br>";
+    }
+}
+?>
