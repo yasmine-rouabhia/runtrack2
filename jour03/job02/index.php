2d300ba9bdcb780e19015258c4e110776739de90
yasmine-rouabhia <yasmine.rouabhia@laplateforme.io>
2021-11-04 17:06:32 +0100

    jour3 job02 fini

---
+++ jour03/job02/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie."; // Chaîne de caractères
+
+for($v = 0; isset($str[$v]); $v++){
+
+// if($v % 2 == 0){// } 
+    echo $str[$v];
+$v++;
+}
+?>
